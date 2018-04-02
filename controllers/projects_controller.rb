ec0bc51b75ae04d4241d4d00e3f34e19b0ef0a30
b09 <code2stack@gmail.com>
2018-04-02 21:08:47 +0100

    project.rb form for new projects is correctly adding new data to db

--- controllers/projects_controller.rb
+++ controllers/projects_controller.rb
@@ -6,3 +6,13 @@ get '/projects' do
   @projects = Project.all()
   erb( :"projects/index" )
 end
+
+get '/projects/new' do
+  erb(:"projects/new")
+end
+
+post '/projects' do
+  new_project = Project.new(params)
+  new_project.save
+  redirect to ("/projects")
+end
